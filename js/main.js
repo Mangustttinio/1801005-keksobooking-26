cd240af6cbda94149be84de0dbac201c3b170411
Mangustttinio <malikov.artem@seznam.cz>
2022-08-06 16:57:30 +0200

    Изменил let на const, сделал стрелочные функции, исправил последнюю функцию

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-let getRandomPositiveFloat = function(a, b, digits = 1) {
+const getRandomPositiveFloat = (a, b, digits = 1) => {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
   const result = Math.random() * (upper - lower) + lower;
   return result.toFixed(digits);
 }
-for (let i = 0; i < 10; i++){
-  console.log(getRandomPositiveFloat(-1.13,-1.29))
-}
 
-let getRandomPositiveInteger = function(a, b) {
+const getRandomPositiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
