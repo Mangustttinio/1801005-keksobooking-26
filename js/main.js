d979853ea41477fd7b2d471a8fb992f78ed61b36
Mangustttinio <malikov.artem@seznam.cz>
2022-08-06 12:28:22 +0200

    Подключил main.js к index.html, сделал функции отвечающие за случайные числа

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let getRandomPositiveFloat = function(a, b, digits = 1) {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+}
+for (let i = 0; i < 10; i++){
+  console.log(getRandomPositiveFloat(-1.13,-1.29))
+}
+
+let getRandomPositiveInteger = function(a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  return Math.floor(result);
+};
