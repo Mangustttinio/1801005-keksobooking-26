73f80e0ce589d886bcba167fbc076faa4757702e
Mangustttinio <malikov.artem@seznam.cz>
2022-08-07 15:18:07 +0200

    Написал константы и функцию для случайного набора удобств помещения

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const LAT_MIN = 35.65000;
+const LAT_MAX = 35.70000;
+const LAT_ROUNDING = 5;
+const LNG_MIN = 139.70000;
+const LNG_MAX = 139.80000;
+const LNG_ROUNDING = 5;
+const DEFAULT_GENERATE_NUM = 10;
+const GUEST_MIN = 1;
+const GUEST_MAX = 100;
+const PRICE_MIN = 1;
+const PRICE_MAX = 1000000;
+const titleList = ['Введите текст', 'Заполните пусты поля', 'Заполните пустую форму', 'Отсутствует информация'];
+const typeOfFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
 const getRandomPositiveFloat = (a, b, digits = 1) => {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
@@ -11,3 +25,16 @@ const getRandomPositiveInteger = (a, b) => {
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
+
+const getFeatures = () => {
+  const featureChoice = [];
+  for (let index = 0; index <= typeOfFeatures.length - 1; index++){
+    if (getRandomPositiveInteger(1,2) === 1){
+      featureChoice.push(typeOfFeatures[index]);
+    }
+  }
+  return featureChoice;
+};
+
+//author = {
+//avatar: `img/avatars/user${value < 10 ? `0${value}` : value}.png`
