8052f7de960aa766f0c440bff9f795a4d4c016e2
Mangustttinio <malikov.artem@seznam.cz>
2022-08-12 22:43:59 +0200

    Написал функции актвации и деактивации формы(продублировал ветку вооизбежании проблемы конфликтов)

---
+++ js/create-data/form-conditions.js
@@ -0,0 +1,28 @@
+const formType = document.querySelector('.ad-form');
+const mapFilterType = document.querySelector('.map__filters');
+const mapFilterChildren = mapFilterType.children;
+const formChildren = formType.children;
+
+const formDeactivation = () => {
+  formType.classList.add('ad-form__disabled');
+  mapFilterType.classList.add('map__filters--disabled');
+  for (const value of mapFilterChildren) {
+    value.setAttribute('disabled', true);
+  }
+  for (const value of formChildren) {
+    value.setAttribute('disabled', true);
+  }
+};
+
+const formActivation = function () {
+  formType.classList.remove('ad-form--disabled');
+  mapFilterType.classList.remove('map__filter--disabled');
+  for (const value of mapFilterChildren) {
+    value.removeAttribute('disabled', true);
+  }
+  for (const value of formChildren) {
+    value.removeAttribute('disabled', true);
+  }
+};
+
+export {formActivation, formDeactivation};
--- js/main.js
+++ js/main.js
@@ -5,7 +5,13 @@ import {
 import {
   generateAds
 } from './create-data/generate-ads.js';
+import {
+  formActivation,
+  formDeactivation
+} from './create-data/form-conditions.js';
 
 const ads = generateAds();
 const card = createPopupElement(ads[1]);
 renderPopup(card);
+
+formDeactivation();
