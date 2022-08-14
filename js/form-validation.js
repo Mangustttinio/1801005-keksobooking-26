889d1c5123421fca31ec469d5bb6a60a2562212c
Mangustttinio <malikov.artem@seznam.cz>
2022-08-14 20:03:50 +0200

    Убрал найденные недочеты

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,3 +1,4 @@
+const form = document.querySelector('.ad-form');
 const roomNumberOption = {
   1: [1],
   2: [1,2],
@@ -8,7 +9,7 @@ const roomNumberOption = {
 const regulateButtons = (selectRoomNumber,selectCapacityOption, selectCapacity) => {
   selectRoomNumber.addEventListener('change', (event) => {
     const capacity = roomNumberOption[event.target.value];
-    for (const iterator of selectCapacityOption) {
+    selectCapacityOption.forEach((iterator) => {
       if (capacity.includes(Number(iterator.value))) {
         iterator.removeAttribute('disabled');
         selectCapacity.value = iterator.value;
@@ -16,12 +17,35 @@ const regulateButtons = (selectRoomNumber,selectCapacityOption, selectCapacity) 
       else {
         iterator.setAttribute('disabled', true);
       }
-    }
+    });
   });
 };
-
 const validateLength = (value) => value.length >= 30 && value.length <= 100;
 
 const validatePrice = (value) => value <= 100000;
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__block',
+  errorTextParent: 'ad-form__block',
+  errorTextClass: 'ad-form__error'
+}
+);
+
+pristine.addValidator(
+  form.querySelector('#title'),
+  validateLength,
+  'от 30 до 100 символов'
+);
+
+pristine.addValidator(
+  form.querySelector('#price'),
+  validatePrice,
+  'превышено допустимое значение цены'
+);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
 
 export {validateLength, validatePrice, regulateButtons};
--- js/main.js
+++ js/main.js
@@ -8,12 +8,10 @@ import {
 import {
   formActivation,
   //formDeactivation
-} from './create-data/form-conditions.js';
+} from './form-conditions.js';
 import {
-  validateLength,
-  validatePrice,
   regulateButtons
-} from './create-data/form-validation.js';
+} from './form-validation.js';
 
 const ads = generateAds();
 const card = createPopupElement(ads[1]);
@@ -25,40 +23,9 @@ const form = document.querySelector('.ad-form');
 const selectCapacity = form.querySelector('#capacity');
 const selectCapacityOption = selectCapacity.querySelectorAll('option');
 const selectRoomNumber = form.querySelector('#room_number');
-//const buttonUpload = document.querySelector('.ad-form__submit');
-
 
 regulateButtons (
   selectRoomNumber,
   selectCapacityOption,
   selectCapacity
 );
-
-const pristine = new Pristine(form, {
-  classTo: 'ad-form__block',
-  errorTextParent: 'ad-form__block',
-  errorTextClass: 'ad-form__error'
-},
-false
-);
-
-pristine.addValidator(
-  form.querySelector('#title'),
-  validateLength,
-  'от 30 до 100 символов'
-);
-
-pristine.addValidator(
-  form.querySelector('#price'),
-  validatePrice,
-  'превышено допустимое значение цены'
-);
-
-form.addEventListener('submit', () => {
-  pristine.validate();
-});
-
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
-});
