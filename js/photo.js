e70aa9e9186c5b87be99ff318012cd35302e912f
Mangustttinio <malikov.artem@seznam.cz>
2022-09-08 19:57:26 +0200

    Проведена работа над ошибками

--- js/photo.js
+++ js/photo.js
@@ -5,34 +5,40 @@ const adFormHeaderPreview = document.querySelector('.ad-form-header__preview');
 const imgForm = adFormHeaderPreview.querySelector('img');
 const adFormUpload = document.querySelector('.ad-form__upload input[type=file]');
 const adFormPhoto = document.querySelector('.ad-form__photo');
+const adFormPhotoContainer = document.querySelector('.ad-form__photo-container');
 
-const preparePhoto = (button, photoArea, image) => {
-  button.addEventListener('change', () => {
-    const file = button.files[0];
-    const fileName = file.name.toLowerCase();
-    const matches = IMG_FILE_TYPES.some((it) => fileName.endsWith(it));
-    photoArea.appendChild(image);
-    if (matches) {
-      image.src = URL.createObjectURL(file);
-    }
-  });
+const preparePhoto = (button) => {
+  const file = button.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = IMG_FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (matches) {
+    return URL.createObjectURL(file);
+  }
 };
 
 const changePhoto = () => {
-  preparePhoto(adFormField, adFormHeaderPreview, imgForm);
+  const src = preparePhoto(adFormField);
+  if (src) {
+    imgForm.src = src;
+  }
 };
 
 const addPhoto = () => {
   const imgElement = document.createElement('img');
-  imgElement.alt = '';
-  imgElement.src = '';
-  adFormPhoto.appendChild(imgElement);
-  preparePhoto(adFormUpload, adFormPhoto, imgElement);
+  const src = preparePhoto(adFormUpload);
+  if (src) {
+    imgElement.src = src;
+  }
+  imgElement.width = 40;
+  imgElement.height = 44;
+  const photo = adFormPhoto.cloneNode();
+  photo.appendChild(imgElement);
+  adFormPhotoContainer.appendChild(photo);
 };
 
 const doAllActionsWithPhoto = () => {
-  addPhoto();
-  changePhoto();
+  adFormUpload.addEventListener('change', changePhoto);
+  adFormField.addEventListener('change', addPhoto);
 };
 
 export {doAllActionsWithPhoto};
